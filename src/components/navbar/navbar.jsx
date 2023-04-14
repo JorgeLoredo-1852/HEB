99ec5b968c3798c226e6b3d1c01caa8ab53d2cd7
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-04-14 12:23:08 -0600

    Added Navbar and Routing

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,24 +3,33 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import styles from './navbar.module.css'
 import { tabs } from './tabs';
+import { useState, useEffect } from 'react';
+
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
-  const [value, setValue] = React.useState('home');
+  const [value, setValue] = useState('/');
+  const router = useRouter();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+  const handleClick = (link) => {
+    router.push(link);
   };
 
   return (
     <div style={{width:"100vw", position:"fixed", bottom:0}}>
     <BottomNavigation value={value} onChange={handleChange}>
         {tabs.map((tab) => (      
-            <BottomNavigationAction
-                className={value == tab.value ? styles.navbarBtnActive : styles.navbarBtn}
-                label={tab.label}
-                value={tab.value}
-                icon={tab.icon}
-            />
+              <BottomNavigationAction
+                  onClick={() => handleClick(tab.link)}
+                  className={value == tab.value ? styles.navbarBtnActive : styles.navbarBtn}
+                  label={tab.label}
+                  value={tab.value}
+                  icon={tab.icon}
+                  key={tab.label}
+              />
         ))}
     </BottomNavigation>
     </div>
