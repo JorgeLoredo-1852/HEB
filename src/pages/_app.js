fde9b74f6e6fdf1d70db8085f5bf9958aa62c8c8
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-04-14 09:14:20 -0600

    Changed navbar file name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import Navbar from '@/components/navbar/Navbar'
+import Navbar from '@/components/navbar/navbar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
