82f04339d45071ed363f786e0c2989c4b4f9b140
guigixd <luisferd321@gmail.com>
2023-04-20 10:42:19 -0600

    Conexion Firebase

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,33 @@ import AppBarHeader from '@/components/navbar/appBarHeader/appBarHeader'
 import HeaderImg from '@/molecules/header/HeaderImg'
 import '@/styles/globals.css'
 import { Poppins } from '@next/font/google'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWT4mLjto-JRexnFS99V5BZNGPE0YBOzs",
+  authDomain: "heb-go.firebaseapp.com",
+  databaseURL: "https://heb-go-default-rtdb.firebaseio.com",
+  projectId: "heb-go",
+  storageBucket: "heb-go.appspot.com",
+  messagingSenderId: "855494035420",
+  appId: "1:855494035420:web:83d130a72d5b73f01db119",
+  measurementId: "G-TYR8D6KKNZ"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+/*
+async function getCities(db) {
+  const citiesCol = collection(db, 'Ingredientes');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
+console.log(getCities(db))*/
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] })
 
