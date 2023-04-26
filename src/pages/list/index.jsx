fb2781b511db02b5f9bb0b3b6cbcfc91d66ee8ee
Pablo Navarro Zepeda <panaze@outlook.com>
2023-04-26 08:47:04 -0600

    Added shoping list component and subtotal/total

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,21 +1,41 @@
-import { useContext } from 'react';
-import ListContext from '@/hooks/ListContext';
+import { useContext } from "react";
+import ListContext from "@/hooks/ListContext";
+import ShoppingList from "@/components/shoppingList/shoppingList";
+import { GetCollection, GetItem } from "../../api/firebase";
+import BigButton from "@/atoms/buttonBig/buttonBig";
 
-const ShoppingList = () => {
+const List = () => {
+  const { listInfo, setListInfo } = useContext(ListContext);
+  const foodList = GetCollection("Ingredientes");
 
-    const { listInfo, setListInfo } = useContext(ListContext)
+  const onClick = () => {
+    //Pending to add route
+  };
 
+  return (
+    /*
+    <div style={{ marginTop: "6rem" }}>
+      {Object.keys(listInfo).map((key) => {
+        return (
+          <div>
+            {key} // {listInfo[key]}
+          </div>
+        );
+      })}
+    </div>*/
 
-    return (
-        <div style={{marginTop:"6rem"}}>
-            {Object.keys(listInfo).map((key) => {
-                return (
-                    <div>
-                        {key} // {listInfo[key]}
-                    </div>
-                )
-            })}
-        </div>)
-}
+    <div style={{ marginTop: "6rem" }}>
+      <ShoppingList foodLists={foodList} />
+      <BigButton
+        color="main"
+        callback={onClick}
+        position="fixed"
+        text="Desbloquear descuentos"
+      />
+    </div>
+  );
+};
 
-export default ShoppingList
+export default List;
+
+//
