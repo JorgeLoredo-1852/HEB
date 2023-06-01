b2870169d5b2a7b573770480a5df1f9592c72a4e
EmilioPFematt <supermageblaziken@gmail.com>
2023-06-01 07:14:22 -0600

    agregar imagenes categorias

--- src/components/categories/arr.js
+++ src/components/categories/arr.js
@@ -3,32 +3,32 @@ export const arr = [
   {
     id: 1,
     name: "Desayunos",
-    img: "/images/food.png"
+    img: "https://cdn7.kiwilimon.com/recetaimagen/34034/39643.jpg"
   },
   {
     id: 2,
     name: "Cenas",
-    img: "/images/mole.png"
+    img: "https://pescadoszaragozaextremadura.es/wp-content/uploads/2023/03/salmon-a-la-parrilla-con-vegetales-asados.jpg"
   },
   {
     id: 3,
     name: "Bebidas",
-    img: "/images/pozole.jpg" 
+    img: "https://static.onecms.io/wp-content/uploads/sites/21/2017/06/27/margarita-clasica-2000.jpg" 
   },
   {
     id: 4,
     name: "Postres",
-    img: "/images/cochinita.jpeg"
+    img: "https://images.cookforyourlife.org/wp-content/uploads/2020/06/Dark-Chocolate-Brownies-shutterstock_112430981.jpg"
   },
   {
     id: 5,
     name: "Japonesa",
-    img: "/images/pescado.jpg"
+    img: "https://www.ahumadosdominguez.es/wp-content/uploads/2022/06/makis-salmon-ahumado-aguacate-sesamo.jpg"
   },
   {
     id: 6,
     name: "Italiana",
-    img: "/images/tlayuda.jpg"
+    img: "https://www.annarecetasfaciles.com/files/pizza-margarita-1-scaled.jpg"
   },
   {
     id:7,
